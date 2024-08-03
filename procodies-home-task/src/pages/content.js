2ae419cf2b8525385b52493722da6a964144e5c0
TomaszMoroz <tmoroz688@gmail.com>
2024-08-03 13:57:36 +0200

    pages: content file

---
+++ procodies-home-task/src/pages/content.js
@@ -0,0 +1,44 @@
+export const content = {
+  headers: {
+    textBox: ['Top Tech Mystery Box', 'at Unbeatable Prices'],
+    infoPanel1: ['Curious about the StockX', 'Mystery Box?'],
+    infoPanel2: ['Why is everyone buzzing', 'about these boxes?']
+  },
+
+  spans: {
+    infoPanel: { target: 'StockX', color: '#006340' }
+  },
+
+  texts: {
+    textBox: ['Fast shipping', 'Easy returns', 'Special discounts for students'],
+    infoPanel1: ['Experience the thrill of unboxing cutting-edge gadgets with Stockx Mystery Boxes. Each box is a treasure trove of high-quality electronics, handpicked to elevate your tech game.',
+      'Expect the unexpected! Our boxes are packed with the latest gadgets, from smartphones to gaming consoles, ensuring you get the best tech surprises every time.'],
+    infoPanel2: ['StockX Mystery Boxes are not just about products; they’re about the experience. Imagine the excitement of unveiling top-tier electronics at a fraction of the cost.',
+      'Our boxes include a variety of premium tech items, making it a must-have for every tech enthusiast. Join the buzz and see why everyone is raving about the unbeatable value and surprise factor of Stockx Mystery Boxes!'
+    ],
+    boxesFooter: 'We accept the following credit cards'
+  },
+
+  infoPanelImages: {
+    panel1: { imageName: 'textbox_1.png', imageWidth: '583px', imageHeight: '583px' },
+    panel2: { imageName: 'textbox_2.png', imageWidth: '583px', imageHeight: '583px' }
+  },
+
+  infoPanelBottomCards: [
+    { header: 'How can I win?', text: 'Thanks to your membership, you also participate in our contest to win the iPhone15. We sellect a winner among every 600 participants. The next winner will be selected on 06.10.2023, and will be notified directly by e-mail. If the reward is out of stock, the winner will receive a similar product of equal or greater value instead.' },
+    { header: 'No hidden fees?', text: 'We ensure that our members are provided with a detailed history of transactions so that they know exactly what they are paying for. Credit card information is only required to facilitate future purchases. There will be no charges on your credit card statement if you do not upgrade to PREMIUM membership and do not make a purchase.' },
+    { header: 'Why do we need your billing information?', text: "By joining this service, you will receive a 5-day trial of our partner's program. If you continue with a subscription beyond the 5-day trial period, you will be charged an amount on your credit card which varies depending on the choice of the tariff. When your contribution is deducted from your credit card or other payment method, you will have access to these services which are reserved exclusively for members of our partner's website." }
+  ],
+
+  boxesFooterImage: { imageName: 'logo_payments.png', imageHeight: '40px' },
+
+  styles: {
+    enterDetails: { hFontSize: '52px', sFontSize: '22px' },
+    textBox: { hColor: '#2D473E', color: '#006340', hFontSize: '48px', tFontSize: '20px' },
+    infoPanel: { hFontSize: '42px', tFontSize: '16px', width: '582px' },
+    infoPanelBottom: { background: '#F5F5F5', tFontSize: '10px' },
+    boxesFooter: { bFontSize: '12px' }
+  },
+
+  separator: ' | '
+}
